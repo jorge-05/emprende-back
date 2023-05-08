3a68840b2164fe8de02a5b19c38080933ba059a6
Jorge <jorgekantlv5555@gmail.com>
2023-05-08 15:11:36 -0500

    Listo para ir a github

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const http = require("http");
-const name = require("./another");
-console.log({ name });
 
 function requestController() {
-  console.log("Hola mundo");
+  console.log("Hola mundo!!!!");
 }
 
 const server = http.createServer(requestController);
